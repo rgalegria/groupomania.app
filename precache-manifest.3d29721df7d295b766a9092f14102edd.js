2b0d2212ac214d6fe2c5f217ded724d8665844c2
rgalegria <59605825+rgalegria@users.noreply.github.com>
2020-10-30 15:00:37 +0100

    Updates

---
+++ precache-manifest.3d29721df7d295b766a9092f14102edd.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbf1ca4a690d40fc220b5a5055f517b",
+    "url": "/groupomania.app/index.html"
+  },
+  {
+    "revision": "0e7d040a48171dc4ae41",
+    "url": "/groupomania.app/static/css/main.4be43bb9.chunk.css"
+  },
+  {
+    "revision": "1d3b13332338aecddcbf",
+    "url": "/groupomania.app/static/js/2.574603be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/groupomania.app/static/js/2.574603be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7d040a48171dc4ae41",
+    "url": "/groupomania.app/static/js/main.763f3427.chunk.js"
+  },
+  {
+    "revision": "2a69e72666a58a447286",
+    "url": "/groupomania.app/static/js/runtime-main.14b6c0d6.js"
+  },
+  {
+    "revision": "1a8a09ecaa6cc7762e125d6f1d9debb6",
+    "url": "/groupomania.app/static/media/agenda-icon.1a8a09ec.svg"
+  },
+  {
+    "revision": "69eb6e06a8ab82e173fbb932039ebbb7",
+    "url": "/groupomania.app/static/media/back-icon.69eb6e06.svg"
+  },
+  {
+    "revision": "5ff4ccf54b17297118b7f6f0bf342574",
+    "url": "/groupomania.app/static/media/background.5ff4ccf5.jpg"
+  },
+  {
+    "revision": "a2cc9e0c0bed96079936e50515b2c967",
+    "url": "/groupomania.app/static/media/categories-icon.a2cc9e0c.svg"
+  },
+  {
+    "revision": "400e2e3e9aeba7cb14424a97e0583373",
+    "url": "/groupomania.app/static/media/clock-icon.400e2e3e.svg"
+  },
+  {
+    "revision": "80a3ebc0c520d48960213463c41a4c52",
+    "url": "/groupomania.app/static/media/close-icon.80a3ebc0.svg"
+  },
+  {
+    "revision": "3849d652e265da9a39cfb14f37718e54",
+    "url": "/groupomania.app/static/media/coffee-icon.3849d652.svg"
+  },
+  {
+    "revision": "e600818e04b97f38f00c2b11c97152d8",
+    "url": "/groupomania.app/static/media/comment-icon.e600818e.svg"
+  },
+  {
+    "revision": "2f8c4bd836feb364bee6582984b4c6ec",
+    "url": "/groupomania.app/static/media/comments-icon.2f8c4bd8.svg"
+  },
+  {
+    "revision": "5ce044d999fec4164d24455946432488",
+    "url": "/groupomania.app/static/media/delete-icon.5ce044d9.svg"
+  },
+  {
+    "revision": "06913d0415c0fed2e7d580917c81b35b",
+    "url": "/groupomania.app/static/media/dislike-icon.06913d04.svg"
+  },
+  {
+    "revision": "75cb8ec9a11f79b98c51ab841eec4dcc",
+    "url": "/groupomania.app/static/media/email-icon.75cb8ec9.svg"
+  },
+  {
+    "revision": "ffc734c4682ddcebb010b178387c208f",
+    "url": "/groupomania.app/static/media/generic_profile_picture.ffc734c4.jpg"
+  },
+  {
+    "revision": "8a9f8e0c72145794eec061ca2cd69755",
+    "url": "/groupomania.app/static/media/home_background_groupomania.8a9f8e0c.jpg"
+  },
+  {
+    "revision": "937fb3c8f589e1f11d48ac80282e636d",
+    "url": "/groupomania.app/static/media/image-icon.937fb3c8.svg"
+  },
+  {
+    "revision": "ab6f795206e618671cf3dce6d9dbb08e",
+    "url": "/groupomania.app/static/media/lastname-icon.ab6f7952.svg"
+  },
+  {
+    "revision": "d8a1ba16931526e6de100697da55dad7",
+    "url": "/groupomania.app/static/media/like-icon.d8a1ba16.svg"
+  },
+  {
+    "revision": "53c46fedcc5612d9e5e73bd25d41cbb9",
+    "url": "/groupomania.app/static/media/linkedin-grey-icon.53c46fed.svg"
+  },
+  {
+    "revision": "06701358339a74a99dd4c1c51f02a870",
+    "url": "/groupomania.app/static/media/linkedin-icon.06701358.svg"
+  },
+  {
+    "revision": "d2e9ba6c5386d532042cb991b13999e2",
+    "url": "/groupomania.app/static/media/login-icon.d2e9ba6c.svg"
+  },
+  {
+    "revision": "9fbde2c26d65cccff8245de57593d744",
+    "url": "/groupomania.app/static/media/logo.9fbde2c2.png"
+  },
+  {
+    "revision": "9c126e27f05d6fa613c7b1ad03cdf2a5",
+    "url": "/groupomania.app/static/media/logotype.9c126e27.svg"
+  },
+  {
+    "revision": "d93f43f1f7281a12b1ad5edfd694b64e",
+    "url": "/groupomania.app/static/media/logout-icon.d93f43f1.svg"
+  },
+  {
+    "revision": "e07316bddaca11d3961e68adf239153f",
+    "url": "/groupomania.app/static/media/menu-icon.e07316bd.svg"
+  },
+  {
+    "revision": "b5ae602ef90368c0a83ae634d8eed67f",
+    "url": "/groupomania.app/static/media/modify-icon.b5ae602e.svg"
+  },
+  {
+    "revision": "353b75753ed526984cebd6ac20207661",
+    "url": "/groupomania.app/static/media/ok-icon.353b7575.svg"
+  },
+  {
+    "revision": "3807eb0c76586fb6041d7702d3571035",
+    "url": "/groupomania.app/static/media/password-icon.3807eb0c.svg"
+  },
+  {
+    "revision": "13fff284efdb12cec4f63b506d301ce1",
+    "url": "/groupomania.app/static/media/person-icon.13fff284.svg"
+  },
+  {
+    "revision": "b8ec1592ba1b9a31ade2177c6d2c2107",
+    "url": "/groupomania.app/static/media/post-icon.b8ec1592.svg"
+  },
+  {
+    "revision": "b39fb4847bed02d77331d592875b1ead",
+    "url": "/groupomania.app/static/media/posts-icon.b39fb484.svg"
+  },
+  {
+    "revision": "56611ebbb537dc7a98e2de3cee734acc",
+    "url": "/groupomania.app/static/media/send-icon.56611ebb.svg"
+  },
+  {
+    "revision": "34cd9cab185b85a1a0c0c57ad05ebdf9",
+    "url": "/groupomania.app/static/media/signup-icon.34cd9cab.svg"
+  },
+  {
+    "revision": "eeaccb418c76a2426a119e1feeeb9a5b",
+    "url": "/groupomania.app/static/media/x-icon.eeaccb41.svg"
+  }
+]);
